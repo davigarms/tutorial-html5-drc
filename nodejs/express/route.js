fb26a1470318477be6b00874955164bfa0bec203
Garms <davi@garms.com>
2015-06-19 22:00:41 -0300

    nodejs, route, rest

--- nodejs/express/route.js
+++ nodejs/express/route.js
@@ -1,10 +1,20 @@
 var express = require('express'),
     app = express();
 
-app.get('/', function(req, res) {
-   res.send ('<h1>Hello from NodeJS</h1>');
+/*app.get('/', function (req, res){
+    res.send('<h1>Hello from NodeJS</h1>');
+});*/
+
+app.use(express.static('public'));
+
+app.get('/service', function (req, res){
+    res.json({data: 'Some data'});
+});
+
+app.get('/service/:name/:last_name', function (req, res){
+    res.send('<h1>Hello '+ req.params.name + ' ' + req.params.last_name + '</h1>');
 });
 
 app.listen(8080);
 
-console.log("Listen Port 8080");
+console.log('Server running on localhost:8080');
